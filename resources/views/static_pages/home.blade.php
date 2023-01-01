bfafc1fbe622c9c24bda9f506b3b37b27b511560
星野 <a4870010@gmail.com>
2023-01-02 02:23:27 +0900

    命名路由

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('title', '主页')
 @section('content')
-    <div class="jumbotron">
+    <div class="h-100 p-5 text-bg-dark rounded-3">
         <h1>Hello Laravel</h1>
         <p class="lead">
         你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-tra">laravel主页</a>
